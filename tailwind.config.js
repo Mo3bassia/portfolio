75eedfdc37ce9597049ab9e10d690b4b8958d1d1
Mohamed Abassia <mo3bassia@gmail.com>
2024-09-23 06:22:08 +0300

    import Tailwind Css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#3E63F9",
+      },
+    },
+  },
+  plugins: [],
+};
