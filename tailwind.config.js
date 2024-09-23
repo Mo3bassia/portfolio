08c4915f0d655f6a636a7d6cac6e3f513c0132fd
Mohamed Abassia <mo3bassia@gmail.com>
2024-09-23 07:07:02 +0300

    import font family and font awesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./assets/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       colors: {
         primaryColor: "#3E63F9",
+      },
+      transitionProperty: {
+        "colors-and-bg": "color, background-color",
+      },
+      fontFamily: {
+        poppins: "Poppins",
       },
     },
   },
